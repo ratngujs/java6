7fa9ebb8a0e78f341d66a37c5d364cfef0536bcb
qhung2003 <nguyenquochung9176@gmail.com>
2023-08-04 21:20:35 +0700

    images

---
+++ SOF306-ASM/bin/src/main/resources/static/assets/js/main.js
@@ -0,0 +1,107 @@
+const app = angular.module("shopping-app", []);
+
+app.run(function ($http, $rootScope) {
+    $http.get(`/rest/auth/authentication`).then(resp => {
+        if (resp.data) {
+            $auth = $rootScope.$auth = resp.data;
+            $http.defaults.headers.common["Authorization"] = $auth.token;
+        }
+    });
+})
+
+app.controller("shopping-ctrl", function ($scope, $http) {
+    var url = "/rest/products";
+    var url1 = "/rest/orders";
+    
+    var sweetalert = function (text) {
+        Swal.fire({
+            icon: "success",
+            title: text,
+            showConfirmButton: false,
+            timer: 2000,
+        });
+    }
+    
+    // Quan ly gio hang
+    $scope.cart = {
+        items: [],
+        // Them sp vao gio hang
+        add(id) {
+            var item = this.items.find(item => item.id == id);
+            if (item) {
+                item.qty++;
+                this.saveToLocalStorage();
+            }
+            else {
+                $http.get(`${url}/${id}`).then(resp => {
+                    resp.data.qty = 1;
+                    this.items.push(resp.data);
+                    this.saveToLocalStorage();
+                })
+            }
+        },
+        // Xoa sp khoi gio hang
+        remove(id) {
+            var index = this.items.findIndex(item => item.id == id);
+            this.items.splice(index, 1);// xoa phan tu
+            this.saveToLocalStorage();
+        },
+        // Xoa sach sp trong gio hang
+        clear() {
+            this.items = [];
+            this.saveToLocalStorage();
+        },
+        // Tinh thanh tien cua 1 sp
+        amt_of(item) { },
+        // Tinh tong so luong cac mat hang trong gio
+        get count() {
+            return this.items
+                .map(item => item.qty)
+                .reduce((total, qty) => total += qty, 0);
+        },
+        // Tong thanh tien cac mat hang trong gio
+        get amount() {
+            return this.items
+                .map(item => item.qty * item.price)
+                .reduce((total, qty) => total += qty, 0);
+        },
+        // Luu gio hang vao localstorage
+        saveToLocalStorage() {
+            var json = JSON.stringify(angular.copy(this.items));
+            localStorage.setItem("cart", json);
+        },
+        // Doc gio hang vao local storage
+        loadFromLocalStorage() {
+            var json = localStorage.getItem("cart");
+            this.items = json ? JSON.parse(json) : [];
+        }
+    }
+    // Lưu giỏ hàng
+    $scope.cart.loadFromLocalStorage();
+    $scope.order = {
+        createDate: new Date(),
+        address: "",
+        account: { username: $("#username").text() },
+        get orderDetails() {
+            return $scope.cart.items.map(item => {
+                return {
+                    product: { id: item.id },
+                    price: item.price,
+                    quantity: item.qty
+                }
+            });
+        },
+        purchase() {
+            var order = angular.copy(this);
+            // Thực hiện đặt hàng
+            $http.post(url1, order).then(resp => {
+            	sweetalert("Đặt hàng thành công!");
+                $scope.cart.clear();// xóa
+                location.href = "/order/detail/" + resp.data.id; // chuyển đến tảng chi tiết dơn hàng
+            }).catch(error => {
+            	sweetalert("Đặt hàng lỗi!");
+                console.log("Error", error);
+            })
+        }
+    }
+})
---
+++ SOF306-ASM/bin/src/main/resources/static/assetss/ctrl/account-ctrl.js
@@ -0,0 +1,133 @@
+app.controller("account-ctrl", function ($scope, $http) {
+    var url = "/rest/accounts";
+    var url1 = "/rest/roles";
+    var url2 = "/rest/upload/images";
+    $scope.roles = [];
+    $scope.items = [];
+    $scope.form = {};
+
+    var sweetalert = function (text) {
+        Swal.fire({
+            icon: "success",
+            title: text,
+            showConfirmButton: false,
+            timer: 2000,
+        });
+    }
+
+    $scope.initialize = function () {
+        //load account
+        $http.get(url).then(resp => {
+            $scope.items = resp.data;
+        });
+        
+        //load roles
+        $http.get(url1).then(resp => {
+            $scope.roles = resp.data;
+        })
+    }
+
+    //khoi dau
+    $scope.initialize();
+
+    //xoa form
+    $scope.reset = function () {
+        $scope.form = {
+            image: 'cloud-upload.jpg',
+        };
+    }
+
+    //hien thi len form
+    $scope.edit = function (item) {
+        $scope.form = angular.copy(item);
+        $(".nav-tabs a:eq(0)").tab('show');
+    }
+
+    //them sp moi
+    $scope.create = function () {
+        var item = angular.copy($scope.form);
+        $http.post(`${url}`, item).then(resp => {
+            resp.data.token = "token";
+            $scope.items.push(resp.data);
+            $scope.reset();
+            sweetalert("Thêm mới thành công!");
+        }).catch(error => {
+            sweetalert("Lỗi thêm mới tài khoản!");
+            console.log("Error", error);
+        });
+    }
+
+    //cap nhat sp
+    $scope.update = function () {
+        var item = angular.copy($scope.form);
+        $http.put(`${url}/${item.id}`, item).then(resp => {
+            var index = $scope.items.findIndex(p => p.id == item.id);
+            $scope.items[index] = item;
+            $scope.reset();
+            sweetalert("Cập nhật tài khoản thành công!");
+        }).catch(error => {
+            sweetalert("Lỗi cập nhật tài khoản!");
+            console.log("Error", error);
+        });
+    }
+
+    //xoa sp
+    $scope.delete = function (item) {
+        $http.delete(`${url}/${item.id}`).then(resp => {
+            var index = $scope.items.findIndex(p => p.id == item.id);
+            $scope.items.splice(index, 1);
+            $scope.reset();
+            sweetalert("Xóa tài khoản thành công!");
+        }).catch(error => {
+            sweetalert("Lỗi xóa tài khoản!");
+            console.log("Error", error);
+        });
+    }
+
+    //upload hinh
+    $scope.imageChanged = function (files) {
+        var data = new FormData();
+        data.append('file', files[0]);
+        $http.post(url2, data, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).then(resp => {
+            $scope.form.image = resp.data.name;
+        }).catch(error => {
+            sweetalert("Lỗi tải lên hình ảnh!");
+            console.log("Error", error);
+        })
+    }
+
+    //phan trang
+    $scope.pager = {
+        page: 0,
+        size: 10,
+        get items() {
+            var start = this.page * this.size;
+            return $scope.items.slice(start, start + this.size);
+        },
+        get count() {
+            return Math.ceil(1.0 * $scope.items.length / this.size)
+        },
+        first() {
+            this.page = 0;
+        },
+        prev() {
+            this.page--;
+            if (this.page < 0) {
+                this.last();
+            }
+        },
+        next() {
+            this.page++;
+            if (this.page >= this.count) {
+                this.first();
+            }
+        },
+        last() {
+            this.page = this.count - 1;
+        }
+    }
+
+});
